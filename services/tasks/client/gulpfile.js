7b0a9dad687d96fde674dc756c65c51327645ca7
George Dean <pair+gdean@pivotal.io>
2016-07-20 13:31:58 -0700

    WIP: Create a real build pipeline
    The current problem is that the bundle.js file has the task-list
    template before Polymer({is: "task-list"})

--- services/tasks/client/gulpfile.js
+++ services/tasks/client/gulpfile.js
@@ -1,8 +1,28 @@
 var gulp = require('gulp');
 var jasmineBrowser = require('gulp-jasmine-browser');
+var webpack = require('gulp-webpack');
 
 gulp.task('jasmine', function() {
     return gulp.src(['src/task_list.html', 'spec/**/*_spec.js'])
         .pipe(jasmineBrowser.specRunner())
         .pipe(jasmineBrowser.server());
+});
+
+gulp.task('build', function() {
+    return gulp.src('src/index.js')
+        .pipe(webpack({
+            watch: true,
+            module: {
+                preLoaders: [
+                    { test: /\/src\/.+\.js$/, loader: 'polymer-loader?templateExtension=html' }
+                ],
+                loaders: [
+                    { test: /\.html$/, loader: "html" }
+                ]
+            },
+            output: {
+                filename: "bundle.js"
+            }
+        }))
+        .pipe(gulp.dest('../build/public'));
 });
---
+++ services/tasks/client/src/index.js
@@ -0,0 +1,1 @@
+require("./task-list/task-list.js");
